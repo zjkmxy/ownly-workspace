19834dec767b1871754b4cc6b5eea63f0843a631
Varun Patil <radialapps@gmail.com>
2025-02-11 10:50:33 -0800

    Home screen

---
+++ src/components/home/Workspace.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="workspace card">
+    <div class="card-content">
+      <div class="block">
+        <div class="media-content">
+          <p class="has-text-weight-bold">{{ name }}</p>
+          <p class="subtitle is-7 mt-1">{{ subtitle }}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        <button class="button is-primary is-fullwidth">Enter</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: String,
+  subtitle: String,
+})
+</script>
+
+<style scoped lang="scss">
+.card,
+.card-content,
+.media-content {
+  overflow: hidden;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,41 @@
-<template></template>
+<template>
+  <section class="hero">
+    <div class="hero-body pb-1">
+      <p class="title">Dashboard</p>
+      <p class="subtitle mt-2">You can create or join one or more workspaces below</p>
+    </div>
+  </section>
 
-<script setup lang="ts"></script>
+  <div class="spacelist">
+    <div class="workspace card">
+      <div class="card-content">
+        <div class="content">
+          <button class="button is-fullwidth mb-2">Create New</button>
+          <button class="button is-primary is-fullwidth">Join Workspace</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<script setup lang="ts">
+import Workspace from '@/components/home/Workspace.vue'
+</script>
+
+<style scoped lang="scss">
+.spacelist {
+  margin: 40px;
+}
+.workspace {
+  display: inline-block;
+  width: 100%;
+  vertical-align: top;
+}
+
+@media (min-width: 600px) {
+  .workspace {
+    width: 250px;
+    margin-right: 20px;
+  }
+}
+</style>
