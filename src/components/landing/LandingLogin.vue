8833ed847b7f690da98ba683959cc923654a24c3
Varun Patil <radialapps@gmail.com>
2025-02-10 16:13:02 -0800

    login: spinner

--- src/components/landing/LandingLogin.vue
+++ src/components/landing/LandingLogin.vue
@@ -1,17 +1,53 @@
 <template>
-  <div class="box wrapper p-4">
-    <div class="header is-size-4 has-text-weight-semibold">Get started</div>
+  <div class="wrapper">
+    <Transition @after-leave="afterTransition">
+      <div class="loading anim-fade has-text-white-bis" v-if="showLoading">
+        <Spinner />
+      </div>
+    </Transition>
 
-    <div class="login mt-2">
-      First, you need an email address to verify your unique identity.
-      <input class="input mt-3" type="email" placeholder="name@email.com" />
+    <Transition @after-leave="afterTransition">
+      <div class="box anim-rtl-fade p-4" v-if="showEmail">
+        <div class="header is-size-4 has-text-weight-semibold">Get started</div>
 
-      <button class="button mt-3 is-primary is-fullwidth">Continue</button>
-    </div>
+        <div class="login mt-2">
+          First, you need an email address to verify your unique identity.
+          <input class="input mt-3" type="email" placeholder="name@email.com" />
+
+          <button class="button mt-3 is-primary is-fullwidth" @click="submitEmail">Continue</button>
+        </div>
+      </div>
+    </Transition>
+
+    <Transition @after-leave="afterTransition">
+      <div class="box anim-rtl-fade p-4" v-if="showCode">
+        <div class="header is-size-4 has-text-weight-semibold">Let's verify you</div>
+
+        <div class="login mt-2">
+          We have sent a verification code to your email address. Enter the code below to continue.
+          <input class="input mt-3" type="text" placeholder="123456" />
+
+          <button class="button mt-3 is-primary is-fullwidth">Continue</button>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import Spinner from '@/components/Spinner.vue'
+
+let showLoading = ref(false)
+let showEmail = ref(true)
+let showCode = ref(false)
+let afterTransition = ref(() => {})
+
+function submitEmail() {
+  afterTransition.value = () => (showLoading.value = true)
+  showEmail.value = false
+}
+</script>
 
 <style scoped lang="scss">
 .wrapper {
