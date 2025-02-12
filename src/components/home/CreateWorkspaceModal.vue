0754b2b9f4532fb7d5346236ced78b1319147f8e
Varun Patil <radialapps@gmail.com>
2025-02-11 18:03:27 -0800

    Create modal

---
+++ src/components/home/CreateWorkspaceModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal is-active anim-fade">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <div class="title is-5 mb-4">Create Workspace</div>
+
+        <div class="field">
+          <label class="label">Dashboard Label</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Marketing Team" />
+          </div>
+          <p class="help">A human-readable title for the workspace on your dashboard</p>
+        </div>
+
+        <div class="field">
+          <label class="label">NDN Name</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="/org/division/team/workspace" />
+          </div>
+          <p class="help">A unique NDN name identifier for the workspace, structured like a path</p>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary">Create</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,16 +10,24 @@
     <div class="workspace card">
       <div class="card-content">
         <div class="content">
-          <button class="button is-fullwidth mb-2">Create New</button>
+          <button class="button is-fullwidth mb-2" @click="showCreate = true">Create New</button>
           <button class="button is-primary is-fullwidth">Join Workspace</button>
         </div>
       </div>
     </div>
   </div>
+
+  <Transition>
+    <CreateWorkspaceModal v-if="showCreate" @close="showCreate = false" />
+  </Transition>
 </template>
 
 <script setup lang="ts">
+import CreateWorkspaceModal from '@/components/home/CreateWorkspaceModal.vue'
 import Workspace from '@/components/home/Workspace.vue'
+import { ref } from 'vue'
+
+const showCreate = ref(false)
 </script>
 
 <style scoped lang="scss">
