c981d1f9161eb72aa59ecf8ff6ba59d2527d71c5
brad-lowe <93014613+brad-lowe@users.noreply.github.com>
2025-08-20 13:08:24 -0700

    Fixed access request notification behavior

--- src/services/ndn.ts
+++ src/services/ndn.ts
@@ -13,7 +13,7 @@ declare global {
   var _yjs_merge_updates: (updates: Uint8Array[]) => Uint8Array;
   var _ndnd_conn_change_js: (connected: boolean, router: string) => void;
   var _ndnd_conn_state: { connected: boolean; router: string };
-  var _access_requests: string[];
+  var _access_requests: [string,string,boolean][]; // boolean value checks whether this access request has already been dealt with to prevent duplicates
 
   var set_ndn: undefined | ((ndn: NDNAPI) => void);
   var ndn_api: NDNAPI;
@@ -149,7 +149,7 @@ class NDNService {
     globalThis._yjs_merge_updates = Y.mergeUpdatesV2;
     globalThis._ndnd_conn_change_js = _ndnd_conn_change_js;
     globalThis._ndnd_conn_state = { connected: false, router: String() };
-    globalThis._access_requests = new Array<string>();
+    globalThis._access_requests = new Array<[string,string,boolean]>();
 
     // Load the Go WASM module
     const go = new Go();
